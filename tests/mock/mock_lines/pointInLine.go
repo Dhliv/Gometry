57d7f8aec0276819d79283f9de703564efb29b5c
David Henao <dvdhenaomartinez@gmail.com>
2023-02-21 12:29:05 -0500

    🩹 Created points with more decimal precision

--- tests/mock/mock_lines/pointInLine.go
+++ tests/mock/mock_lines/pointInLine.go
@@ -7,8 +7,8 @@ import (
 
 var (
 	PointsInLine []*models.Point = []*models.Point{
-		S16, S17, S23, S11, S11, S21, S13, S22, models.NewPoint(5.8779601, 11.358834),
-		models.NewPoint(-1.229508, 1.47540983),
+		S16, S17, S23, S11, S11, S21, S13, S22, models.NewPoint(5.8779599523077, 11.3588342584615),
+		models.NewPoint(-1.2295080836066, 1.4754099303279),
 	}
 	PointsOutsideLine []*models.Point = []*models.Point{
 		S2, S13, S11, S22, D, S11, S4, S18, S16, S13,
